f474550629f637e6826780f04e5a71a16d10579e
davidciliberto <davidciliberto@gmail.com>
2019-12-04 16:32:12 -0300

    ReactPacientesV

--- src/App.js
+++ src/App.js
@@ -2,26 +2,84 @@ import React, { Component } from "react";
 import "./bootstrap.min.css";
 import Header from "./components/header";
 import NuevaCita from "./components/NuevaCita";
+import ListaCitas from "./components/ListaCitas";
+import Cita from "./components/cita";
+
 
 class App extends Component {
-    state = {};
-    render() {
-        return ( <
-            div className = "container" >
-            <
-            Header titulo = "Administrador Pacientes Veterinaria" / >
-            <
-            div className = "row" >
-            <
-            div className = "col-md-10 mx-auto" >
-            <
-            NuevaCita / >
-            <
-            /div> < /
-            div > <
-            /div>
-        );
+    state = {
+        citas: []
     }
-}
+    crearNuevaCita = datos => {
+       // Copiar el State actual
 
+      const citas = [...this.state.citas, datos];
+       console.log(datos);
+       // Agregar el nuevo state
+
+        this.setState({
+            citas 
+        })
+       }
+
+       // Cuando la aplicacion carga
+
+       componentDidMount() {
+            const citasLS = localStorage.getItem('citas');
+            if (citasLS) {
+                this.setState({
+                    citas: JSON.parse(citasLS)
+                })
+            }
+       }
+
+       // Cuando eliminamos o agregamos nueva cita
+
+       componentDidUpdate() {
+           localStorage.setItem('citas', JSON.stringify(this.state.citas));
+       }
+
+       // Eliminar citas del State
+
+       eliminarCita = id => {
+          
+        // tomar una copia del State 
+
+        const citasActuales = [...this.state.citas];
+
+        // Utilizar filter para sacar el elemento @id del arreglo
+
+        const citas = citasActuales.filter(Cita => Cita.id !== id )
+
+        // Actualizar el State
+
+        this.setState({
+            citas
+        })
+
+       }
+
+    render() {
+        return (
+            <div className="container" >
+            <Header titulo="Administrador Pacientes Veterinaria" />
+            <div className="row" >
+                <div className="col-md-10 mx-auto" >
+                    <NuevaCita 
+                    crearNuevaCita = {this.crearNuevaCita}
+                     />
+                    </div> 
+                    <div className="mt-5 col-md-10 mx-auto">
+                        <ListaCitas 
+                        citas={this.state.citas}
+                        eliminarCita={this.eliminarCita}
+                        />
+                    </div>
+                </div>
+            </div>
+ );
+                    }
+                }
+                
 export default App;
+
--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -1,144 +1,172 @@
 import React, { Component } from "react";
+import uuid from 'uuid';
+import PropTypes from 'prop-types';
+
+
+const stateInicial = {
+  cita: {
+    Mascota: '',
+    Propietario: '',
+    Fecha: '',
+    Hora: '',
+    Sintomas: ''
+  },
+  error: false,
+}
 
 class NuevaCita extends Component {
   state = {
-      cita : {
-        Mascota: '',
-        Propietario: '',
-        Fecha: '',
-        Hora: '',
-        Sintomas: '' 
-      },
-      error: false,
+    ...stateInicial
   }
   // Cuando el usuario escribe en los inputs
-  
-  handleChange = e => {
-      console.log(e.target.name + ': ' + e.target.value);
 
-      // Colocar lo que el usuario escribe en el state
-      this.setState({
-          cita: {
-              ...this.state.cita,
-              [e.target.name] : e.target.value
-          }
-      })
+  handleChange = e => {
+    console.log(e.target.name + ': ' + e.target.value);
+
+    // Colocar lo que el usuario escribe en el state
+    this.setState({
+      cita: {
+        ...this.state.cita,
+        [e.target.name]: e.target.value
+      }
+    })
   }
 
   // Cuando el usuario envia el formulario
 
   handleSubmit = e => {
-        e.preventDefault();
+    e.preventDefault();
 
-        // Extraer los valores del state 
-        
-        const {Mascota, Propietario, Fecha, Hora, Sintomas} = this.state.cita;
+    // Extraer los valores del state 
 
-        // Validar que todos los campos esten llenos
+    const { Mascota, Propietario, Fecha, Hora, Sintomas } = this.state.cita;
 
-        if(Mascota === '' || Propietario === '' || Fecha === '' || Hora === '' || Sintomas === '') {
-            this.setState({
-                error: true
-            });
+    // Validar que todos los campos esten llenos
 
-            // Detener la ejecucion 
-            return;
-        }
+    if (Mascota === '' || Propietario === '' || Fecha === '' || Hora === '' || Sintomas === '') {
+      this.setState({
+        error: true
+      });
 
-        // Agregar la cita al state de App
+      // Detener la ejecucion 
+      return;
+    }
 
+    // Generar Objetos con los datos
 
+    const nuevaCita = {...this.state.cita};
+     nuevaCita.id = uuid();
+
+    // Agregar la cita al state de App
+    this.props.crearNuevaCita(nuevaCita)
+
+    // Colocar en el State el stateInicial
+
+    this.setState({
+      ...stateInicial
+    })
   }
 
   render() {
+
+// Extraer Valor del State
+
+
+const { error } = this.state;
+
+
     return (
       <div className="card mt-5 py-5">
-        <div className="card-body">
-          <h2 className="card-title text-center mb-5">
-            Llena el formulario para nueva cita
-          </h2>
-          <form 
-          onSubmit={this.handleSubmit}
-          >
+        <div className="card-body" >
+          <h2 className="card-title text-center mb-5" >
+            Llena el formulario para nueva cita </h2>
 
-            <div className="form-group row">
-              <label className="col-sm-4 col-lg-2 col-form-label">
-                Nombre Mascota
-              </label>
-              <div className="col-sm-8 col-lg-10">
-                <input
-                  id="Mascota"
-                  type="text"
-                  className="form-control"
-                  placeholder="Nombre Mascota"
-                  name="Mascota"
-                  onChange={this.handleChange}
-                  value={this.state.cita.Mascota}
-                />
-              </div>
-            </div>
-            <div className="form-group row">
-              <label className="col-sm-4 col-lg-2 col-form-label">
-                Nombre Dueño
-              </label>
-              <div className="col-sm-8 col-lg-10">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Nombre Dueño Mascota"
-                  name="Propietario"
-                  onChange={this.handleChange}
-                  value={this.state.cita.Propietario}
-                />
-              </div>
-            </div>
-            <div className="form-group row">
-              <label className="col-sm-4 col-lg-2 col-form-label">Fecha </label>
-              <div className="col-sm-8 col-lg-4">
-                <input 
-                type="date" 
-                className="form-control" 
-                name="Fecha" 
-                onChange={this.handleChange}
-                value={this.state.cita.Fecha}
+            { error ? <div className="alert alert-danger mt-2 mb-5 text-center">Todos los campos son obligatorios</div> : null }
+
+            <form onSubmit={this.handleSubmit} >
+              <div className="form-group row" >
+                <label className="col-sm-4 col-lg-2 col-form-label" >
+                  Nombre Mascota
+                  </label>
+                <div className="col-sm-8 col-lg-10" >
+                  <input 
+                    id="Mascota"
+                    type="text"
+                    className="form-control"
+                    placeholder="Nombre Mascota"
+                    name="Mascota"
+                    onChange={this.handleChange}
+                    value={this.state.cita.Mascota}
                   />
-              </div>
-              <label className="col-sm-4 col-lg-2 col-form-label">Hora </label>
-              <div className="col-sm-8 col-lg-4">
-                <input 
-                type="time"
-                 className="form-control" 
-                 placeholder="Nombre Mascota"
-                 name="Hora" 
-                 onChange={this.handleChange}
-                 value={this.state.cita.hora}
-                 />
+                  </div>
+                  </div> 
+                  <div className="form-group row" >
+                    <label className="col-sm-4 col-lg-2 col-form-label" >
+                      Nombre Dueño </label>
+                    <div className="col-sm-8 col-lg-10" >
+                      <input 
+                        type="text"
+                        className="form-control"
+                        placeholder="Nombre Dueño Mascota"
+                        name="Propietario"
+                        onChange={this.handleChange}
+                        value={this.state.cita.Propietario}
+                      /> </div>
+                  </div>
+                  <div className="form-group row" >
+                    <label className="col-sm-4 col-lg-2 col-form-label" >
+                      Fecha
+                                 </label>
+                    <div className="col-sm-8 col-lg-4" >
+                      <input 
+                        type="date"
+                        className="form-control"
+                        name="Fecha"
+                        onChange={this.handleChange}
+                        value={this.state.cita.Fecha}
+                      />
+                    </div>
+                    <label className="col-sm-4 col-lg-2 col-form-label" >
+                      Hora
+                  </label>
+                    <div className="col-sm-8 col-lg-4" >
+                      <input 
+                        type="time"
+                        className="form-control"
+                        placeholder="Hora"
+                        name="Hora"
+                        onChange={this.handleChange}
+                        value={this.state.cita.Hora}
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group row" >
+                    <label className="col-sm-4 col-lg-2 col-form-label" >
+                      Sintomas
+                      </label>
+                    <div className="col-sm-8 col-lg-10" >
+                      <textarea className="form-control"
+                        name="Sintomas"
+                        placeholder="Describe los Sintomas"
+                        onChange={this.handleChange}
+                        value={this.state.cita.Sintomas}>
+                      </textarea>
+                    </div>
+                  </div>
+                  <input
+                   type="submit"
+                    className="py-3 mt-2 btn btn-success btn-block"
+                    value="Agregar Nueva Cita"
+                  />
+                  </form>
               </div>
             </div>
-            <div className="form-group row">
-              <label className="col-sm-4 col-lg-2 col-form-label">
-                Sintomas
-              </label>
-              <div className="col-sm-8 col-lg-10">
-                <textarea
-                  className="form-control"
-                  name="Sintomas"
-                  placeholder="Describe los Sintomas"
-                  onChange={this.handleChange}
-                  value={this.state.cita.sintomas}
-                ></textarea>
-              </div>
-            </div>
-            <input
-              type="submit"
-              className="py-3 mt-2 btn btn-success btn-block"
-              value="Agregar Nueva Cita"
-            />
-          </form>
-        </div>
-      </div>
-    );
-  }
-}
+            );
+        }
+    }
 
+    NuevaCita.propTypes = {
+      crearNuevaCita : PropTypes.func.isRequired
+    }
+    
 export default NuevaCita;
